22bf5f46a091fc943adbac17f7a3d4b6fbefee7b
Shibi Suriya P <shibisuriya.work@gmail.com>
2025-04-30 12:33:33 +0530

    fix: updated siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/shibisuriya/my-blog/',
+  siteRepo: 'https://github.com/shibisuriya/blog/',
   siteLogo: '/static/images/logo.pngg',
   socialBanner: '/static/images/twitter-card.png',
   // mastodon: 'https://mastodon.social/@mastodonuser',
