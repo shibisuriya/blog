a6e98819fb260c3bfada91d5fcf327b6cb3d0901
Shibi Suriya P <shibisuriya.work@gmail.com>
2025-04-30 17:06:44 +0530

    Prevented rendering BlogStatus component in production

--- components/BlogStatus.tsx
+++ components/BlogStatus.tsx
@@ -5,6 +5,10 @@ interface Props {
 function BlogStatus(props: Props) {
   const { isPublished } = props
 
+  if (process.env.NODE_ENV === 'production') {
+    return <></>
+  }
+
   return isPublished ? (
     <span className="m-1 inline-block rounded-lg bg-green-500 p-2 text-xs text-white">
       PUBLISHED
