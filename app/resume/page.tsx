6f7926407c4b7c47a3589e5f755f0843c3316e54
Shibi Suriya P <shibisuriya.work@gmail.com>
2025-04-30 19:44:17 +0530

    resume test - 6

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,8 @@
 import { genPageMetadata } from 'app/seo'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import { Resume, allResumes } from 'contentlayer/generated'
+import Pdf from './pdf.svg'
+import Docs from './docs.svg'
 
 export const metadata = genPageMetadata({ title: 'Resume', description: "Shibi Suriya's resume" })
 
@@ -14,6 +16,30 @@ export default function ResumePage() {
             Resume
           </h1>
         </div>
+        <div>
+          <h2 className="mb-6 pt-10 text-2xl font-extrabold">Download</h2>
+          <div className="mb-12">
+            <div className="flex gap-3">
+              <div>
+                <a
+                  href="https://shibisuriya.github.io/blog/static/shibi-resume.pdf"
+                  target="_blank"
+                >
+                  <Pdf width="50px" height="50px" />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://shibisuriya.github.io/blog/static/shibi-resume.docx"
+                  target="_blank"
+                >
+                  <Docs width="50px" height="50px" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 xl:divide-y-0 dark:divide-gray-700">
           <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
             <div className="prose dark:prose-invert max-w-none pt-10 pb-8">
