8c96eee0e8e0de40c98302940dc66bde43f54bdb
Shibi Suriya <shibisuriya.work@gmail.com>
2025-07-17 21:34:55 +0530

    updated export resume script

--- scripts/export-resume.ts
+++ scripts/export-resume.ts
@@ -9,17 +9,37 @@ async function exportResume() {
   fs.mkdirSync(OUTPUT_DIR, { recursive: true })
 
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   })
   const page = await browser.newPage()
 
-  console.log(`⏳ Visiting ${RESUME_URL}...`)
   await page.goto(RESUME_URL, { waitUntil: 'networkidle0' })
 
   const pdfPath = path.join(OUTPUT_DIR, 'shibi-resume.pdf')
 
-  // await browser.close()
+  await page.evaluate(() => {
+    const resumeElem = document.querySelector('#resume')
+    if (!resumeElem) {
+      throw new Error('Resume element not found in DOM.')
+    }
+    resumeElem.classList.add('pl-10', 'pr-10', 'pt-10', 'pb-10')
+    document.body.innerHTML = ''
+    document.body.appendChild(resumeElem)
+  })
+
+  const updatedElement = await page.$('html')
+  const boundingBox = await updatedElement!.boundingBox()
+
+  await page.pdf({
+    path: pdfPath,
+    printBackground: true,
+    margin: { left: '0cm', right: '0cm', top: '0cm', bottom: '0cm' },
+    width: `${boundingBox!.width}px`,
+    height: `${boundingBox!.height}px`,
+  })
+
+  await browser.close()
 }
 
 exportResume()
