21e357159353984afcb60d48664d6997ebfdfcdf
Shibi Suriya <shibisuriya.work@gmail.com>
2025-07-18 11:30:01 +0530

    Updated siteUrl

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Welcome to my blog!',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/shibisuriya/blog/',
+  siteUrl: 'https://shibisuriya.github.io/blog',
+  siteRepo: 'https://github.com/shibisuriya/blog',
   siteLogo: '/static/images/logo.pngg',
   socialBanner: '/static/images/twitter-card.png',
   // mastodon: 'https://mastodon.social/@mastodonuser',
