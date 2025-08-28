d3bbc393d39abedda26bc40282def2b4915732ba
shibisuriya <shibisuriya.work@gmail.com>
2025-08-28 13:25:23 +0530

    fix: layout of header changed

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,8 +24,8 @@ const Header = () => {
           )}
         </div>
       </Link>
-      <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
+      <div className="flex items-center gap-4">
+        <div className="no-scrollbar hidden justify-between gap-4 overflow-x-auto md:flex">
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
