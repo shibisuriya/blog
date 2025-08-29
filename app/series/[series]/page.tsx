f13d6fac34ff599c36ae066ae2fe6790ab4dc715
shibisuriya <shibisuriya.work@gmail.com>
2025-08-29 15:09:16 +0530

    implemented series

--- app/series/[series]/page.tsx
+++ app/series/[series]/page.tsx
@@ -3,9 +3,8 @@ import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 import { Posts } from '@/components/Posts'
 import { slug } from 'github-slugger'
-import { Pagination } from '@/components/Pagination'
-
-const POSTS_PER_PAGE = 5
+import Link from '@/components/Link'
+import seriesData from 'app/series-data.json'
 
 export const metadata = genPageMetadata({ title: 'Series' })
 
@@ -16,19 +15,43 @@ export default async function Page(props: { params: Promise<{ series: string }> 
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.series && slug(post.series) === series))
   )
-  const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = filteredPosts.slice(0, POSTS_PER_PAGE)
-  const pagination = {
-    currentPage: 1,
-    totalPages: totalPages,
-  }
+  const sortedSeriesData = Object.keys(seriesData).sort((a, b) => seriesData[b] - seriesData[a])
 
   return (
-    <div>
-      <Posts posts={filteredPosts} showTags={false} showPublishedOn={false} />
-      {pagination && pagination.totalPages > 1 && (
-        <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
-      )}
-    </div>
+    <>
+      <div>
+        <div className="pt-6 pb-6"></div>
+        <div className="flex sm:space-x-24">
+          <div className="hidden h-full max-h-screen max-w-[280px] min-w-[280px] flex-wrap overflow-auto rounded-sm bg-gray-50 pt-5 shadow-md sm:flex dark:bg-gray-900/70 dark:shadow-gray-800/40">
+            <div className="px-6 py-4">
+              <ul>
+                {sortedSeriesData.map((s) => {
+                  return (
+                    <li key={s} className="my-3">
+                      {series === slug(s) ? (
+                        <h3 className="text-primary-500 inline px-3 py-2 text-sm font-bold uppercase">
+                          {`${s} (${seriesData[s]})`}
+                        </h3>
+                      ) : (
+                        <Link
+                          href={`/series/${slug(s)}`}
+                          className="hover:text-primary-500 dark:hover:text-primary-500 px-3 py-2 text-sm font-medium text-gray-500 uppercase dark:text-gray-300"
+                          aria-label={`View posts tagged ${s}`}
+                        >
+                          {`${s} (${seriesData[s]})`}
+                        </Link>
+                      )}
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+          <div>
+            <Posts posts={filteredPosts} showTags={false} showPublishedOn={false} />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
